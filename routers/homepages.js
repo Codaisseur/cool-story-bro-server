42f25c0db08a30c69c959983207bed36c59a7d2c
Reinoptland <reinoptland@gmail.com>
2020-03-22 11:28:02 +0100

    Post stories

--- routers/homepages.js
+++ routers/homepages.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const auth = require("../auth/middleware");
 const Homepage = require("../models").homepage;
+const Story = require("../models").story;
 
 const router = new Router();
 
@@ -17,6 +18,36 @@ router.patch("/:id", auth, async (req, res) => {
   await homepage.update({ title, description, backgroundColor, color });
 
   return res.status(200).send({ homepage });
+});
+
+router.post("/:id/stories", auth, async (req, res) => {
+  const homepage = await Homepage.findByPk(req.params.id);
+  console.log(homepage);
+
+  if (homepage === null) {
+    return res.status(404).send({ message: "This homepage does not exist" });
+  }
+
+  if (!homepage.userId === req.user.id) {
+    return res
+      .status(403)
+      .send({ message: "You are not authorized to update this homepage" });
+  }
+
+  const { name, imageUrl, content } = req.body;
+
+  if (!name) {
+    return res.status(400).send({ message: "A story must have a name" });
+  }
+
+  const story = await Story.create({
+    name,
+    imageUrl,
+    content,
+    homepageId: homepage.id
+  });
+
+  return res.status(201).send({ message: "Story created", story });
 });
 
 module.exports = router;
